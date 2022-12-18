3b263ebfe4a1262998aaf44d9a7f7f909d4c5826
alex <alex@pdp7.net>
2022-12-18 14:46:00 +0100

    Add emacs bash config

---
+++ emacs/emacs.bash
@@ -0,0 +1,6 @@
+# source this file from your bash startup script
+
+alias emacs="emacsclient --create-frame -t"
+export ALTERNATE_EDITOR=""
+export EDITOR="emacsclient -t"
+export VISUAL="emacsclient -c -a emacs"
