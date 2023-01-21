593880316179c1be3655193a774827a1a3178a70
alex <alex@pdp7.net>
2023-01-21 23:02:22 +0100

    Manage dnsmasq on h1
    * Add support for FreeIPA DNS entries

--- personal_infra/puppet/site/h1.pdp7.net.pp
+++ personal_infra/puppet/site/h1.pdp7.net.pp
@@ -1,6 +1,7 @@
 node 'h1.pdp7.net' {
   class {'proxmox':}
   class {'dns_dhcp':}
+  class {'freeipa::dnsmasq':}
 
   # TODO: ugly; tinc scripts require this :(
   package {'net-tools':}
--- personal_infra/puppet/site/h2.pdp7.net.pp
+++ personal_infra/puppet/site/h2.pdp7.net.pp
@@ -1,2 +1,10 @@
 node 'h2.pdp7.net' {
+  class {'dns_dhcp':}
+  class {'freeipa::dnsmasq':}
+
+  file {'/etc/dnsmasq.d/static.conf':
+    content => "dhcp-host=freeswitch,10.42.42.3,freeswitch
+host-record=h2.h2.int.pdp7.net,10.42.42.1
+",
+  }    
 }
