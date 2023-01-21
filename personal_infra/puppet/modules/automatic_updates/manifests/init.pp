5c8bd89e050c2d6b572c09daefbae0e5ad388c47
alex <alex@pdp7.net>
2023-01-21 22:22:48 +0100

    Extend support for automatic_updates, fail if unsupported

--- personal_infra/puppet/modules/automatic_updates/manifests/init.pp
+++ personal_infra/puppet/modules/automatic_updates/manifests/init.pp
@@ -2,4 +2,32 @@ class automatic_updates {
   if ($facts['os']['family'] == 'Debian') {
     package {["unattended-upgrades", "apt-listchanges"]:}
   }
+  elsif ($facts['os']['family'] == 'RedHat') {
+    if ($facts['os']['release']['major'] == '7') {
+      package {'yum-cron':}
+      ->
+      file {"/etc/yum/yum-cron.conf":
+        content => epp('automatic_updates/yum-cron.conf'),
+      }
+      ~>
+      service {'yum-cron':
+        ensure => running,
+        enable => true,
+      }
+    }
+    elsif ($facts['os']['release']['major'] == '8') {
+      package {'dnf-automatic':}
+      ->
+      service {'dnf-automatic-install.timer':
+        ensure => running,
+        enable => true,
+      }
+    }
+    else {
+      fail($facts['os']['release']['major'])
+    }
+  }
+  else {
+    fail($facts['os'])
+  }
 }
