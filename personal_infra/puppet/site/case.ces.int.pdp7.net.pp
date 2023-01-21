f48f254f914de6248a2de2269724c40e5b90b1b9
alex <alex@pdp7.net>
2023-01-21 23:33:28 +0100

    Manage dnsmasq in case

--- personal_infra/puppet/site/case.ces.int.pdp7.net.pp
+++ personal_infra/puppet/site/case.ces.int.pdp7.net.pp
@@ -1,2 +1,12 @@
 node 'case.ces.int.pdp7.net' {
+  class {'dns_dhcp':}
+  ->
+  file {'/etc/dnsmasq.d/static.conf':
+    content => 'host-record=router,router.ces.int.pdp7.net,10.17.19.1
+host-record=tplink,tplink.ces.int.pdp7.net,10.17.19.2
+host-record=case.ces.int.pdp7.net,case,10.17.19.3
+',
+  }
+
+  class {'freeipa::dnsmasq':}
 }
