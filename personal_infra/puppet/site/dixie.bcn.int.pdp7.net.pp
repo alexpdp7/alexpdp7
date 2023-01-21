68bc42df8f92544dee32b6b19a9d4dfdd86a03d9
alex <alex@pdp7.net>
2023-01-21 23:44:11 +0100

    Manage dnsmasq on dixie

--- personal_infra/puppet/site/dixie.bcn.int.pdp7.net.pp
+++ personal_infra/puppet/site/dixie.bcn.int.pdp7.net.pp
@@ -1,2 +1,12 @@
 node 'dixie.bcn.int.pdp7.net' {
+  class {'dns_dhcp':}
+  file {'/etc/dnsmasq.d/static.conf':
+    content => "host-record=router,router.bcn.int.pdp7.net,192.168.76.1
+host-record=archerc7,archerc7.bcn.int.pdp7.net,192.168.76.6
+host-record=dixie.bcn.int.pdp7.net,dixie,192.168.76.2
+dhcp-option=121,10.0.0.0/8,192.168.76.2
+",
+    notify => Service["dnsmasq"],
+  }
+  class {'freeipa::dnsmasq':}
 }
