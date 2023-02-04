5c86a772c576840032f2f73cba3f58acc5701e23
alex <alex@pdp7.net>
2023-02-04 18:34:31 +0100

    Add support for fixing IP addresses

--- personal_infra/puppet/modules/dns_dhcp/manifests/init.pp
+++ personal_infra/puppet/modules/dns_dhcp/manifests/init.pp
@@ -1,5 +1,9 @@
 class dns_dhcp {
-  $domain = lookup("network.dns_dhcp.domain")
+  $domain = lookup('network.dns_dhcp.domain')
+
+  $hostvars = lookup('hostvars')
+  $fixed_host_vars = $hostvars.filter |$host, $vars| { $vars['network'] and $vars['network']['dhcp_server'] == $facts["networking"]["fqdn"] }
+  $fixed_hosts = Hash($fixed_host_vars.map |$host, $vars| { [$host.match(/^[a-z0-9]+/)[0], $vars['network']['ip'] ] })
 
   package {'dnsmasq':}
   ->
@@ -12,6 +16,7 @@ class dns_dhcp {
     content => epp('dns_dhcp/internal', {
       'dns_dhcp' => lookup("network.dns_dhcp"),
       'dns_other_server_defs' => $dns_other_server_defs,
+      'fixed_hosts' => $fixed_hosts,
     }),
   }
   ~>
