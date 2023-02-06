e39e095112aa5806e03ea186eb856e69c9df1a1b
alex <alex@pdp7.net>
2023-02-06 21:51:16 +0100

    Remove bad weak dependency

--- personal_infra/puppet/modules/freeipa/manifests/server.pp
+++ personal_infra/puppet/modules/freeipa/manifests/server.pp
@@ -1,3 +1,8 @@
 class freeipa::server {
   package {['ipa-server', 'ipa-server-dns', 'ipa-healthcheck']:}
+
+  # weak dependency that does not work on LXC[I
+  package {'low-memory-monitor':
+    ensure => purged,
+  }
 }
