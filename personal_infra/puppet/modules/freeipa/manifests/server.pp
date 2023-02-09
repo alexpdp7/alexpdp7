0cfa56eab81b5836d10a330c93ff1397748bdd54
alex <alex@pdp7.net>
2023-02-09 21:36:10 +0100

    Activate ipa-healthcheck

--- personal_infra/puppet/modules/freeipa/manifests/server.pp
+++ personal_infra/puppet/modules/freeipa/manifests/server.pp
@@ -1,5 +1,10 @@
 class freeipa::server {
   package {['ipa-server', 'ipa-server-dns', 'ipa-healthcheck']:}
+  ~>
+  service {'ipa-healthcheck':
+    ensure => running,
+    enable => true,
+  }
 
   # weak dependency that does not work on LXC[I
   package {'low-memory-monitor':
