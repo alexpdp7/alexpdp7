9b3e07f3f5ace3ff339dc1f79734373f161b5620
alex <alex@pdp7.net>
2023-02-19 02:59:11 +0100

    Allow dhcp hosts and regular hosts

--- personal_infra/puppet/modules/dns_dhcp/manifests/init.pp
+++ personal_infra/puppet/modules/dns_dhcp/manifests/init.pp
@@ -2,8 +2,12 @@ class dns_dhcp {
   $domain = lookup('network.dns_dhcp.domain')
 
   $hostvars = lookup('hostvars')
-  $fixed_host_vars = $hostvars.filter |$host, $vars| { $vars['network'] and $vars['network']['dhcp_server'] == $facts["networking"]["fqdn"] }
-  $fixed_hosts = Hash($fixed_host_vars.map |$host, $vars| { [$host.match(/^[a-z0-9]+/)[0], $vars['network']['ip'] ] })
+  $fixed_dhcp_host_vars = $hostvars.filter |$host, $vars| { $vars['network'] and $vars['network']['dhcp_server'] == $facts["networking"]["fqdn"] }
+  $fixed_dhcp_hosts = Hash($fixed_dhcp_host_vars.map |$host, $vars| { [$host.match(/^[-a-z0-9]+/)[0], $vars['network']['ip'] ] })
+
+  $fixed_host_vars = $hostvars.filter |$host, $vars| { $vars['network'] and $vars['network']['register_dns_server'] == $facts["networking"]["fqdn"] }
+  $fixed_hosts = Hash($fixed_host_vars.map |$host, $vars| { [$host.match(/^[-a-z0-9]+/)[0], $vars['network']['ip'] ] })
+
 
   package {'dnsmasq':}
   ->
@@ -16,6 +20,7 @@ class dns_dhcp {
     content => epp('dns_dhcp/internal', {
       'dns_dhcp' => lookup("network.dns_dhcp"),
       'dns_other_server_defs' => $dns_other_server_defs,
+      'fixed_dhcp_hosts' => $fixed_dhcp_hosts,
       'fixed_hosts' => $fixed_hosts,
     }),
   }
