6947b1ee7a9c377d2453914eeeab273bc823afa8
alex <alex@pdp7.net>
2023-02-19 23:08:11 +0100

    Disable pac services that only fail here (!)

--- personal_infra/puppet/site/case.ces.int.pdp7.net.pp
+++ personal_infra/puppet/site/case.ces.int.pdp7.net.pp
@@ -7,4 +7,9 @@ host-record=tplink,tplink.ces.int.pdp7.net,10.17.19.2
 host-record=case.ces.int.pdp7.net,case,10.17.19.3
 ',
   }
+
+  service {['sssd-pac.service', 'sssd-pac.socket']:
+    ensure => stopped,
+    enable => mask,
+  }
 }
