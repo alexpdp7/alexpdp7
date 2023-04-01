77e3435742fb9f05f290ce08b93fda42aa560a2a
alex <alex@pdp7.net>
2023-04-01 15:58:08 +0200

    Add Podman support

---
+++ personal_infra/puppet/modules/podman/manifests/init.pp
@@ -0,0 +1,17 @@
+class podman($user, $storage_driver) {
+  package {'podman':}
+
+  file {['/etc/subuid', '/etc/subgid']:
+    content => "${user}:100000:65536\n",
+  }
+
+  exec {"/usr/bin/sed -i 's/driver = \".*\"/driver = \"${storage_driver}\"/g' /etc/containers/storage.conf":
+    require => Package['podman'],
+    unless => "/usr/bin/grep 'driver = \"${storage_driver}\"' /etc/containers/storage.conf",
+  }
+
+  exec {"/usr/bin/sed -i 's|#mount_program = \"/usr/bin/fuse-overlayfs\"|mount_program = \"/usr/bin/fuse-overlayfs\"|g' /etc/containers/storage.conf":
+    require => Package['podman'],
+    unless => "/usr/bin/grep '^#mount_program = \"/usr/bin/fuse-overlayfs\"'",
+  }
+}
