cd295891a3d13c53b50768c8905c81eeb32a06bb
alex <alex@pdp7.net>
2023-06-03 11:47:41 +0200

    Use Ipsilon now in EPEL

--- personal_infra/puppet/modules/ipsilon/manifests/init.pp
+++ personal_infra/puppet/modules/ipsilon/manifests/init.pp
@@ -1,15 +1,5 @@
 class ipsilon {
-  package {'ipsilon-tools-ipa':
-    source => 'https://kojipkgs.fedoraproject.org//packages/ipsilon/3.0.4/5.el8/noarch/ipsilon-tools-ipa-3.0.4-5.el8.noarch.rpm',
-  }
-
-  package {'ipsilon-openidc':
-    source => 'https://kojipkgs.fedoraproject.org//packages/ipsilon/3.0.4/5.el8/noarch/ipsilon-openidc-3.0.4-5.el8.noarch.rpm',
-  }
-
-  package {'ipsilon-authpam':
-    source => 'https://kojipkgs.fedoraproject.org//packages/ipsilon/3.0.4/5.el8/noarch/ipsilon-authpam-3.0.4-5.el8.noarch.rpm',
-  }
+  package {['ipsilon-tools-ipa', 'ipsilon-openidc']:}
 
   service {'httpd':
     ensure => running,
