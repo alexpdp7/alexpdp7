e6ae2ea374dbf7c75fba49a749658519e6c9c9d5
alex <alex@pdp7.net>
2023-06-18 12:21:37 +0200

    Hack sshd Kerberos issues

--- personal_infra/puppet/site/h1.pdp7.net.pp
+++ personal_infra/puppet/site/h1.pdp7.net.pp
@@ -5,6 +5,14 @@ node 'h1.pdp7.net' {
   # TODO: ugly; tinc scripts require this :(
   package {'net-tools':}
 
+  # https://lists.fedorahosted.org/archives/list/freeipa-users@lists.fedorahosted.org/thread/EZSM6LQPSNRY4WA52IYVR46RSXIDU3U7/
+  # SSH hack
+  file {'/etc/ssh/sshd_config.d/weak-gss.conf':
+    content => "GSSAPIStrictAcceptorCheck no\n",
+  }
+  ~>
+  service {'sshd':}
+
   class {'proxmox::proxy':
     mail => lookup('mail.root_mail'),
     base_hostname => lookup('network.public_hostname'),
