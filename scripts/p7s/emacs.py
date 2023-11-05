b7daf11b2819049b11690fbc9ef745713d3c77fe
alex <alex@pdp7.net>
2023-11-05 12:09:41 +0100

    Update to use superconfigure

--- scripts/p7s/emacs.py
+++ scripts/p7s/emacs.py
@@ -1,16 +1,31 @@
 import pathlib
+import shutil
 import textwrap
+import urllib.request
+
+
+COSMO_BUILD_URL = "https://github.com/alexpdp7/superconfigure/releases/download/cleanup/editor.zip"
 
 
 def setup_emacs():
+    cache = pathlib.Path.home() / ".cache"
+    editor_zip = cache / "editor.zip"
+    urllib.request.urlretrieve(COSMO_BUILD_URL, editor_zip)
+
+    superconfigure_cache = cache / "superconfigure"
+    shutil.unpack_archive(editor_zip, superconfigure_cache)
+
+    for b in (superconfigure_cache / "bin").glob("*"):
+        b.chmod(0o755)
+
     bin = pathlib.Path.home() / ".local" / "bin"
     emacs = bin / "emacs"
-    emacs.write_text(textwrap.dedent("""
-    #!/bin/sh
+    emacs.write_text(textwrap.dedent(f"""
+        #!/bin/sh
 
-    ~/.cache/cosmo/bin/emacsclient --create-frame -t "$@" || {
-        ~/.cache/cosmo/bin/emacs --daemon --user=""
-        ~/.cache/cosmo/bin/emacsclient --create-frame -t "$@"
-    }
-    """))
+        {superconfigure_cache}/bin/emacsclient --create-frame -t "$@" || {{
+            {superconfigure_cache}/bin/emacs --daemon --user=""
+            {superconfigure_cache}/bin/emacsclient --create-frame -t "$@"
+        }}
+    """).lstrip())
     emacs.chmod(0o755)
