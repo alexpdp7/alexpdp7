5a7b0d95591334781493baa26ed9bd2e8c895136
alex <alex@pdp7.net>
2023-11-05 15:02:09 +0100

    Add pip for elpy

--- personal_infra/puppet/site/ws.h1.int.pdp7.net.pp
+++ personal_infra/puppet/site/ws.h1.int.pdp7.net.pp
@@ -4,7 +4,7 @@ node 'ws.h1.int.pdp7.net' {
     storage_driver => 'zfs',
   }
 
-  package {['pipx', 'isync', 'weechat', 'rclone', 'fuse', 'rsync', 'sshpass', 'bash-completion', 'gnutls-utils']:}
+  package {['pipx', 'isync', 'weechat', 'rclone', 'fuse', 'rsync', 'sshpass', 'bash-completion', 'gnutls-utils', 'python3-pip']:}
 
   class {'remote_desktop':}
 }
