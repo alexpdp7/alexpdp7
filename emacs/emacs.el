12ce7509fe5aedec5d1d934a7a42c20ec5268d37
alex <alex@pdp7.net>
2023-11-09 19:01:16 +0100

    Disable Helm file notification features not working in Cosmo

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -54,7 +54,8 @@
   (global-set-key (kbd "M-x") #'helm-M-x)
   (global-set-key (kbd "C-x C-f") #'helm-find-files)
   (global-set-key (kbd "C-x C-b") #'helm-mini)
-  (setq helm-ff-skip-boring-files t))
+  (setq helm-ff-skip-boring-files t)
+  (setq helm-ff-use-notify nil))  ; not working in Cosmopolitan
 
 ;; LSP base for Rust and Java
 (use-package lsp-mode
