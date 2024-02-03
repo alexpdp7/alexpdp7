cfabe8d2784383f5bd8e53602032d7b1006c094f
alex <alex@pdp7.net>
2024-02-03 17:53:24 +0100

    Add db-update command

--- scripts/p7s/distrobox.py
+++ scripts/p7s/distrobox.py
@@ -2,9 +2,20 @@ import os
 import subprocess
 
 
+IMAGE="quay.io/alexpdp7/workstation:latest"
+NAME="workstation-latest"
+
+
 def create():
-    subprocess.run(["distrobox", "create", "-i", "quay.io/alexpdp7/workstation:latest"], check=True)
+    subprocess.run(["distrobox", "create", "-i", IMAGE], check=True)
 
 
 def enter():
-    os.execvp("distrobox", ["distrobox", "enter", "workstation-latest"])
+    os.execvp("distrobox", ["distrobox", "enter", NAME])
+
+
+def update():
+    subprocess.run(["distrobox", "rm", "-f", NAME], check=True)
+    subprocess.run(["podman", "rmi", IMAGE], check=True)
+    subprocess.run(["podman", "pull", IMAGE], check=True)
+    subprocess.run(["distrobox", "create", "-i", IMAGE], check=True)
