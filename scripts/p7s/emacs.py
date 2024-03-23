69100cfaeb89811a29924f7bd320a601807bfc0d
alex <alex@pdp7.net>
2024-03-23 19:51:49 +0100

    Also symlink configuration

--- scripts/p7s/emacs.py
+++ scripts/p7s/emacs.py
@@ -6,3 +6,4 @@ import p7s
 def setup_emacs():
     p7s.BASHRC_D.mkdir(parents=True, exist_ok=True)
     subprocess.run(["ln", "-s", (pathlib.Path(__file__).parent.parent.parent / "emacs" / "emacs.bash").absolute(), p7s.BASHRC_D / "emacs.bash"], check=True)
+    subprocess.run(["ln", "-s", (pathlib.Path(__file__).parent.parent.parent / "emacs" / "emacs.el").absolute(), pathlib.Path.home() / ".emacs"], check=True)
