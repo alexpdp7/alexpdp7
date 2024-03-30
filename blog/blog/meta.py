47728a09db39d9454c418255f4f31f6816ec0431
alex <alex@pdp7.net>
2024-03-30 12:03:43 +0100

    Add /2024/03/bienvenidos-al-fediverso.gmi

--- blog/blog/meta.py
+++ blog/blog/meta.py
@@ -4,6 +4,7 @@ HOST = None
 SCHEMA = None
 
 LINKS = (
+    ("@yo@alex.femto.pub", "https://alex.femto.pub/@yo@alex.femto.pub/"),
     ("GitHub", "https://github.com/alexpdp7/"),
     ("LinkedIn", "https://es.linkedin.com/in/alexcorcoles"),
     ("Project Euler", "https://projecteuler.net/profile/koalillo.png"),
