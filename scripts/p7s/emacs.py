c9a7eb098ae8892f9e2324084fd210bcb6682c35
alex <alex@pdp7.net>
2024-05-16 11:32:45 +0200

    Make idempotent

--- scripts/p7s/emacs.py
+++ scripts/p7s/emacs.py
@@ -5,5 +5,7 @@ import p7s
 
 def setup_emacs():
     p7s.BASHRC_D.mkdir(parents=True, exist_ok=True)
+    (p7s.BASHRC_D / "emacs.bash").unlink()
     subprocess.run(["ln", "-s", (pathlib.Path(__file__).parent.parent.parent / "emacs" / "emacs.bash").absolute(), p7s.BASHRC_D / "emacs.bash"], check=True)
+    (pathlib.Path.home() / ".emacs").unlink()
     subprocess.run(["ln", "-s", (pathlib.Path(__file__).parent.parent.parent / "emacs" / "emacs.el").absolute(), pathlib.Path.home() / ".emacs"], check=True)
