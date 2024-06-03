f3cd23837609c8fecfef1e21ddc31cd885a697e4
Alex <alex.corcoles@veecle.io>
2024-06-03 21:33:01 +0200

    Support Ubuntu 24.04

--- emacs/emacs.bash
+++ emacs/emacs.bash
@@ -1,5 +1,13 @@
 # source this file from your bash startup script
 
-alias emacs="emacs --emacs-appimage-run-as emacsclient --create-frame -t"
-export ALTERNATE_EDITOR=""
-export EDITOR="emacs --emacs-appimage-run-as emacsclient -t"
+test -f /usr/bin/emacs && {
+    alias emacs="emacsclient --create-frame -t"
+    export ALTERNATE_EDITOR=""
+    export EDITOR="emacsclient -t"
+}
+
+test -f ~/.local/bin/emacs && {
+    alias emacs="emacs --emacs-appimage-run-as emacsclient --create-frame -t"
+    export ALTERNATE_EDITOR=""
+    export EDITOR="emacs --emacs-appimage-run-as emacsclient -t"
+}
