41318c1481016df8cd3c31763d351a0745ee2f07
Alex <alex.corcoles@veecle.io>
2024-06-05 17:39:32 +0200

    Support Debian

--- scripts/p7s/bash.py
+++ scripts/p7s/bash.py
@@ -11,8 +11,16 @@ def is_ubuntu_2404():
     return platform.freedesktop_os_release().get("VERSION_CODENAME") == "noble"
 
 
+def is_debian_bookworm():
+    return platform.freedesktop_os_release().get("VERSION_CODENAME") == "bookworm"
+
+
+def is_debian_based():
+    return is_ubuntu_2204() or is_ubuntu_2404() or is_debian_bookworm()
+
+
 def setup_bash():
-    if is_ubuntu_2204() or is_ubuntu_2404():
+    if is_debian_based():
         # clone the handy ~/.bashrc.d from Fedora
         bash_aliases = pathlib.Path.home() / ".bash_aliases"
         bash_aliases.write_text(textwrap.dedent("""
