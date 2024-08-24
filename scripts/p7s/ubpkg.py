fc19bf24362b33122f2ef9e866720c95915aebca
alex <alex@pdp7.net>
2024-08-24 14:28:46 +0200

    Support ubpkg multiarch

--- scripts/p7s/ubpkg.py
+++ scripts/p7s/ubpkg.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import platform
 import shutil
 
 import httpx
@@ -7,5 +8,5 @@ import httpx
 
 def setup_ubpkg():
     ubpkg = pathlib.Path.home() / ".local" / "bin" / "ubpkg"
-    ubpkg.write_bytes(httpx.get("https://github.com/alexpdp7/ubpkg/releases/latest/download/ubpkg-linux", follow_redirects=True).content)
+    ubpkg.write_bytes(httpx.get(f"https://github.com/alexpdp7/ubpkg/releases/latest/download/ubpkg-linux-{platform.machine()}", follow_redirects=True).content)
     os.chmod(ubpkg, 0o700)
