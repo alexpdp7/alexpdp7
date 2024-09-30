c6339383bc4a8b4cb5995748951b43466316978c
alex <alex@pdp7.net>
2024-09-30 20:55:04 +0200

    Add note about pinning

--- scripts/p7s/emacs.py
+++ scripts/p7s/emacs.py
@@ -1,5 +1,6 @@
 import pathlib
 import subprocess
+import textwrap
 
 import p7s
 
@@ -9,3 +10,11 @@ def setup_emacs():
     subprocess.run(["ln", "-s", (pathlib.Path(__file__).parent.parent.parent / "emacs" / "emacs.bash").absolute(), p7s.BASHRC_D / "emacs.bash"], check=True)
     (pathlib.Path.home() / ".emacs").unlink(missing_ok=True)
     subprocess.run(["ln", "-s", (pathlib.Path(__file__).parent.parent.parent / "emacs" / "emacs.el").absolute(), pathlib.Path.home() / ".emacs"], check=True)
+
+    print(textwrap.dedent("""
+        On Debian Bookworm, create /etc/apt/preferences.d/emacs with:
+
+        Package: emacs-*
+        Pin: release a=stable-backports
+        Pin-Priority: 500
+    """).lstrip())
