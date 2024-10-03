dbc90011085398253da9c41577752de82eab4b2a
alex <alex@pdp7.net>
2024-10-03 21:17:14 +0200

    Move net-tools install to basic_software

--- personal_infra/puppet/modules/basic_software/manifests/init.pp
+++ personal_infra/puppet/modules/basic_software/manifests/init.pp
@@ -1,5 +1,5 @@
 class basic_software {
-  package {['less', 'mlocate', 'traceroute', 'nmap', 'tree', 'tar', 'screen', 'git']:}
+  package {['less', 'mlocate', 'traceroute', 'nmap', 'tree', 'tar', 'screen', 'git', 'net-tools']:}
 
   if($facts['os']['family'] == 'RedHat') {
     package {'which':}
