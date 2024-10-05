e039dbcf0ffe6c8ca0154b73861dabc060be2362
alex <alex@pdp7.net>
2024-10-05 11:47:38 +0200

    Remove step which is now automated

--- scripts/p7s/emacs.py
+++ scripts/p7s/emacs.py
@@ -10,11 +10,3 @@ def setup_emacs():
     subprocess.run(["ln", "-s", (pathlib.Path(__file__).parent.parent.parent / "emacs" / "emacs.bash").absolute(), p7s.BASHRC_D / "emacs.bash"], check=True)
     (pathlib.Path.home() / ".emacs").unlink(missing_ok=True)
     subprocess.run(["ln", "-s", (pathlib.Path(__file__).parent.parent.parent / "emacs" / "emacs.el").absolute(), pathlib.Path.home() / ".emacs"], check=True)
-
-    print(textwrap.dedent("""
-        On Debian Bookworm, create /etc/apt/preferences.d/emacs with:
-
-        Package: emacs-*
-        Pin: release a=stable-backports
-        Pin-Priority: 500
-    """).lstrip())
