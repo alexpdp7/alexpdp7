c29856b3421c66862bf01bf0662c9b2dc9c2430d
alex <alex@pdp7.net>
2024-10-07 21:26:16 +0200

    Move pipx, rsync, bash-completion, emacs-nox from workstation to basic_software

--- personal_infra/puppet/modules/basic_software/manifests/init.pp
+++ personal_infra/puppet/modules/basic_software/manifests/init.pp
@@ -1,7 +1,22 @@
 class basic_software {
-  package {['less', 'mlocate', 'traceroute', 'nmap', 'tree', 'tar', 'screen', 'git', 'net-tools']:}
+  package {['less', 'mlocate', 'traceroute', 'nmap', 'tree', 'tar', 'screen', 'git', 'net-tools', 'pipx', 'rsync', 'bash-completion']:}
 
   if($facts['os']['family'] == 'RedHat') {
     package {'which':}
+  }
+
+  if ($facts['os']['family'] == 'Debian') {
+    file {'/etc/apt/preferences.d/90_emacs':
+      content => @(EOT)
+      Package: src:emacs
+      Pin: release n=bookworm-backports
+      Pin-Priority: 990
+      | EOT
+      ,
+    }
+    ~>
+    Exec["/usr/bin/apt update"]
+    ->
+    package {'emacs-nox':}
   }
 }
