bccd3c48b0faf65aa0af598090ea786804a34c0e
alex <alex@pdp7.net>
2024-10-13 10:44:06 +0200

    Handle VPN routing in dixie instead of router
    This seems to solve sockets dropping after 30s.
    
    Also use a proper heredoc.

--- personal_infra/puppet/site/maelcum.mad.int.pdp7.net.pp
+++ personal_infra/puppet/site/maelcum.mad.int.pdp7.net.pp
@@ -1,10 +1,14 @@
 node 'maelcum.mad.int.pdp7.net' {
   class {'dns_dhcp':}
   file {'/etc/dnsmasq.d/static.conf':
-    content => 'host-record=router,router.mad.int.pdp7.net,10.34.10.1
-dhcp-host=d8:8c:79:1a:11:59,chromecast,10.34.10.3
-host-record=maelcum.mad.int.pdp7.net,maelcum,10.34.10.2
-',
+    content => @(EOT)
+    host-record=router,router.mad.int.pdp7.net,10.34.10.1
+    dhcp-host=d8:8c:79:1a:11:59,chromecast,10.34.10.3
+    host-record=maelcum.mad.int.pdp7.net,maelcum,10.34.10.2
+
+    dhcp-option=option:classless-static-route,192.168.76.0/24,10.34.10.2,10.43.43.0/24,10.34.10.2,10.17.19.0/24,10.34.10.2
+    | EOT
+    ,
     notify => Service['dnsmasq'],
   }
 }
