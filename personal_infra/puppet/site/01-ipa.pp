976dc407d07a16a89a0764e4ed7e53a116375ad7
alex <alex@pdp7.net>
2024-10-13 12:47:06 +0200

    Move FreeIPA service masking from Proxmox to Debian

--- personal_infra/puppet/site/01-ipa.pp
+++ personal_infra/puppet/site/01-ipa.pp
@@ -15,6 +15,18 @@ if $facts['os']['family'] == 'Debian' and $facts['os']['release']['major'] == "1
   }
 }
 
+if $facts['os']['family'] == 'Debian' and $facts['os']['release']['major'] == "12" {
+  # TODO
+  service {['sssd-ssh.socket', 'sssd-pam-priv.socket', 'sssd-nss.socket', 'sssd-sudo.socket', 'sssd-pam.socket']:
+    ensure => stopped,
+    enable => mask,
+  }
+  ~>
+  exec {'/usr/bin/systemctl reset-failed':
+    refreshonly => true,
+  }
+}
+
 package {$ipa_client_package:}
 package {'sudo':}
 
