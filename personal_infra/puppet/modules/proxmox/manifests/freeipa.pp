eaef85ce2662fb08b3ebb1254845faac0f5f5094
alex <alex@pdp7.net>
2024-10-13 12:47:06 +0200

    Move FreeIPA service masking from Proxmox to Debian

--- personal_infra/puppet/modules/proxmox/manifests/freeipa.pp
+++ personal_infra/puppet/modules/proxmox/manifests/freeipa.pp
@@ -4,14 +4,4 @@ class proxmox::freeipa {
   file {['/etc/subuid', '/etc/subgid']:
     content => epp('proxmox/freeipa_subxid', {'freeipa' => lookup('freeipa')}),
   }
-
-  # TODO
-  service {['sssd-ssh.socket', 'sssd-pam-priv.socket', 'sssd-nss.socket', 'sssd-sudo.socket', 'sssd-pam.socket']:
-    ensure => stopped,
-    enable => mask,
-  }
-  ~>
-  exec {'/usr/bin/systemctl reset-failed':
-    refreshonly => true,
-  }
 }
