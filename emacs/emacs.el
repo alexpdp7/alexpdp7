d49a2621140f84c8e372d406fc402b4acc0a762d
alex <alex@pdp7.net>
2024-10-20 18:10:11 +0200

    Configure elfeed

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -145,3 +145,15 @@
 
 (use-package zmq :ensure t)
 (use-package jupyter :ensure t)
+
+(use-package elfeed :ensure t)
+(use-package elfeed-protocol
+  :ensure t
+  :config
+  (setq elfeed-use-curl t)
+  (elfeed-set-timeout 36000)
+  (setq elfeed-protocol-enabled-protocols '(fever))
+  (elfeed-protocol-enable)
+  (setq elfeed-protocol-feeds '(("fever+https://alex@miniflux.pdp7.net"
+				 :api-url "https://miniflux.pdp7.net/fever/"
+				 :password-file "~/.miniflux.fever.pw"))))
