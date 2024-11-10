7b236efd8ba778d43ad290ca861e868af4a32695
alex <alex@pdp7.net>
2024-11-10 12:21:27 +0100

    Fix properly IPA issues

--- personal_infra/puppet/site/01-ipa.pp
+++ personal_infra/puppet/site/01-ipa.pp
@@ -15,6 +15,18 @@ if $facts['os']['family'] == 'Debian' and $facts['os']['release']['major'] == "1
   }
 }
 
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1026008
+if $facts['os']['family'] == 'Debian' and $facts['os']['release']['major'] == "12" {
+  Package[$ipa_client_package]
+  ->
+  service {['sssd-ssh.socket', 'sssd-nss.socket', 'sssd-sudo.socket', 'sssd-pam-priv.socket']:
+    ensure => stopped,
+    enable => mask,
+  }
+  ~>
+  Exec['/usr/bin/systemctl reset-failed']
+}
+
 package {$ipa_client_package:}
 package {'sudo':}
 
@@ -24,7 +36,9 @@ if 'lxc' in lookup("group_names") {
     enable => mask,
   }
   ~>
-  exec {'/usr/bin/systemctl reset-failed':
-    refreshonly => true,
-  }
+  Exec['/usr/bin/systemctl reset-failed']
+}
+
+exec {'/usr/bin/systemctl reset-failed':
+  refreshonly => true,
 }
