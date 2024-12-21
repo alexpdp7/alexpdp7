a8c67fdd4bd6a1102076f9a7223b8d468f6c41ec
alex <alex@pdp7.net>
2024-12-21 12:46:43 +0100

    Tweak more sssd/FreeIPA/Debian stuff

--- personal_infra/puppet/site/hideo.mad.int.pdp7.net.pp
+++ personal_infra/puppet/site/hideo.mad.int.pdp7.net.pp
@@ -3,4 +3,14 @@ node 'hideo.mad.int.pdp7.net' {
   class {'incus':}
   class {'steam':}
   package {['zfs-dkms', 'sanoid']:}
+
+  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1026008
+  Package[$ipa_client_package]
+  ->
+  service {['sssd-pam-priv.socket", "sssd-sudo.socket", "sssd-nss.socket']:
+    ensure => stopped,
+    enable => mask,
+  }
+  ~>
+  Exec['/usr/bin/systemctl reset-failed']
 }
