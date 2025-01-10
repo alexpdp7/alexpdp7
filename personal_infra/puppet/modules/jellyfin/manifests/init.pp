0e015ab6f4d6c1b438cb4d0468c4576464f54876
alex <alex@pdp7.net>
2025-01-10 11:40:16 +0000

    Unbork jellyfin module

--- personal_infra/puppet/modules/jellyfin/manifests/init.pp
+++ personal_infra/puppet/modules/jellyfin/manifests/init.pp
@@ -1,5 +1,11 @@
 class jellyfin {
-    exec {'/usr/bin/apt install -y apt-transport-https && wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add - && echo "deb [arch=amd64] https://repo.jellyfin.org/debian bookworm main" | sudo tee /etc/apt/sources.list.d/jellyfin.list && apt update && apt install -y jellyfin':
-        creates => '/usr/share/doc/jellyfin/README.Debian',
-    }
+  package {'extrepo':}
+  ->
+  exec {'/usr/bin/extrepo enable jellyfin':
+    creates => '/etc/apt/sources.list.d/extrepo_jellyfin.sources',
+  }
+  ~>
+  Exec["/usr/bin/apt update"]
+  ->
+  package {'jellyfin':}
 }
