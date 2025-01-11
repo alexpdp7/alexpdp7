e44b48b5c230cfc6057f3aff08d85786ab9577d9
alex <alex@pdp7.net>
2025-01-11 20:30:49 +0100

    Also add .fur (nic.fur)

--- personal_infra/puppet/modules/dns_dhcp/manifests/opennic.pp
+++ personal_infra/puppet/modules/dns_dhcp/manifests/opennic.pp
@@ -3,8 +3,8 @@ class dns_dhcp::opennic {
   ->
   file {'/etc/dnsmasq.d/opennic':
     content => @(EOT)
-    server=/bbs/chan/cyb/dyn/geek/gopher/indy/libre/neo/null/o/oss/oz/parody/pirate/94.247.43.254
-    server=/bbs/chan/cyb/dyn/geek/gopher/indy/libre/neo/null/o/oss/oz/parody/pirate/152.53.15.127
+    server=/bbs/chan/cyb/dyn/geek/gopher/indy/libre/neo/null/o/oss/oz/parody/pirate/fur/94.247.43.254
+    server=/bbs/chan/cyb/dyn/geek/gopher/indy/libre/neo/null/o/oss/oz/parody/pirate/fur/152.53.15.127
     | EOT
     ,
   }
