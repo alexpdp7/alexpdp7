e412dbb8a63507a0ff3ff5eb1f3fdd354138194a
alex <alex@pdp7.net>
2025-05-02 11:30:10 +0200

    Fix misbehaving Kobo

--- personal_infra/puppet/site/maelcum.mad.int.pdp7.net.pp
+++ personal_infra/puppet/site/maelcum.mad.int.pdp7.net.pp
@@ -7,7 +7,9 @@ node 'maelcum.mad.int.pdp7.net' {
     dhcp-host=d8:8c:79:1a:11:59,chromecast,10.34.10.3
     host-record=maelcum.mad.int.pdp7.net,maelcum,10.34.10.2
 
-    dhcp-option=option:classless-static-route,192.168.76.0/24,10.34.10.2,10.43.43.0/24,10.34.10.2,10.17.19.0/24,10.34.10.2
+    dhcp-option=tag:!noroutes,option:classless-static-route,192.168.76.0/24,10.34.10.2,10.43.43.0/24,10.34.10.2,10.17.19.0/24,10.34.10.2
+    # Kobo
+    dhcp-host=a4:3c:d7:39:c6:29,set:noroutes
     | EOT
     ,
     notify => Service['dnsmasq'],
