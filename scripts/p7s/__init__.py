84689cd316bb940b700c8758204e1ef6c48a0b54
alex <alex@pdp7.net>
2025-08-10 16:55:02 +0200

    Fix wrapper

--- scripts/p7s/__init__.py
+++ scripts/p7s/__init__.py
@@ -4,7 +4,7 @@ import os, pathlib, shutil, subprocess, sys
 def nix_portable(command):
     def main():
         os.environ["NP_RUNTIME"] = "bwrap"
-        sys.exit(subprocess.run([shutil.which("nix-portable"), "nix", "run", f"nixpkgs#{command}"] + sys.argv[1:]).returncode)
+        sys.exit(subprocess.run([shutil.which("nix-portable"), "nix", "run", f"nixpkgs#{command}", "--"] + sys.argv[1:]).returncode)
     return main
 
 
