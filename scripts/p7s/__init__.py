4c15b787432e2cb41a0cc63fe4cdac81679faec4
alex <alex@pdp7.net>
2025-08-10 16:42:20 +0200

    Also run ledger with nix-portable

--- scripts/p7s/__init__.py
+++ scripts/p7s/__init__.py
@@ -1,4 +1,11 @@
-import pathlib
+import os, pathlib, shutil, subprocess, sys
+
+
+def nix_portable(command):
+    def main():
+        os.environ["NP_RUNTIME"] = "bwrap"
+        sys.exit(subprocess.run([shutil.which("nix-portable"), "nix", "run", f"nixpkgs#{command}"] + sys.argv[1:]).returncode)
+    return main
 
 
 BASHRC_D = pathlib.Path.home() / ".bashrc.d"
