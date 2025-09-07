2a907585e053a56997f013cdf25eaf86f8931a5c
alex <alex@pdp7.net>
2025-09-07 10:43:59 +0200

    Migrate to Migadu

--- scripts/p7s/mail/mbsync.py
+++ scripts/p7s/mail/mbsync.py
@@ -55,3 +55,30 @@ def mbsync_yahoo(login, password, store_path):
         Expunge Both
         SyncState *
     """).lstrip()
+
+
+def mbsync_migadu(login, password, store_path):
+    return textwrap.dedent(f"""
+        IMAPAccount {login}
+        Host imap.migadu.com
+        SSLType IMAPS
+        User {login}
+        Pass {password}
+        PipelineDepth 5
+
+        IMAPStore {login}-remote
+        Account {login}
+
+        MaildirStore {login}-local
+        SubFolders Verbatim
+        Path {store_path}/{login}/
+        Inbox {store_path}/{login}/Inbox
+
+        Channel {login}
+        Far :{login}-remote:
+        Near :{login}-local:
+        Patterns *
+        Create Both
+        Expunge Both
+        SyncState *
+    """).lstrip()
