8f7ef8762f4b65b88c0967de845bf9c4e5272008
alex <alex@pdp7.net>
2025-09-20 10:37:38 +0200

    On Debian 13, PaperWM now installs easily!

--- scripts/p7s/paperwm.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-import subprocess
-
-
-def setup_paperwm():
-    install = (pathlib.Path(__file__).parent.parent / "PaperWM" / "install.sh")
-    subprocess.run([install], check=True)
