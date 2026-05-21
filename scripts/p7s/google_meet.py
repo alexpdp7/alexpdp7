2d07d4327a77b1a16fc4835f4549fcfe8b0b714a
Alex <alex.corcoles@veecle.io>
2026-05-21 16:33:51 +0200

    Refine google-meet tools

--- scripts/p7s/google_meet.py
+++ scripts/p7s/google_meet.py
@@ -1,6 +1,32 @@
 import subprocess
 
-# Needs the https://github.com/ChrisRegado/streamdeck-googlemeet/releases/latest browser extension and websocat
+from p7s import multiline_string as _
+from p7s import systemd
+
+"""
+Needs:
+
+* https://github.com/ChrisRegado/streamdeck-googlemeet/releases/latest
+* https://github.com/vi/wsbroad
+* https://github.com/vi/websocat/
+"""
+
+
+def install_stream_deck_google_meet_wsbroad_server():
+    systemd.create_user_unit("stream-deck-google-meet-wsbroad.service", _("""
+    [Unit]
+    Description=Server for streamdeck-googlemeet browser extension based on wsbroad
+
+    [Service]
+    ExecStart=/home/alex/.local/bin/wsbroad 127.0.0.1:2394
+
+    [Install]
+    WantedBy=default.target
+    """))
+
+    systemd.reload()
+    systemd.enable_now("stream-deck-google-meet-wsbroad.service")
+
 
 def toggle_mute():
     subprocess.run([
@@ -8,5 +34,5 @@ def toggle_mute():
         "--text",
         "--oneshot",
         'literal:{"event":"toggleMic"}',
-        "ws-listen:127.0.0.1:2394"
+        "ws://127.0.0.1:2394"
     ], check=True)
