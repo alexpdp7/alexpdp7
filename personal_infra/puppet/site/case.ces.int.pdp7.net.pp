e569d1ee75c74a9831ad286904e320b177b4599e
alex <alex@pdp7.net>
2026-09-05 20:44:50 +0200

    Update DNS configuration

--- personal_infra/puppet/site/case.ces.int.pdp7.net.pp
+++ personal_infra/puppet/site/case.ces.int.pdp7.net.pp
@@ -1,3 +1,11 @@
 node 'case.ces.int.pdp7.net' {
+  file {'/etc/resolv.conf':
+    content => @(EOT)
+    domain ces.int.pdp7.net
+    search ces.int.pdp7.net
+    nameserver 10.17.19.4
+    | EOT
+    ,
+  }
   # nmbd does not start correctly when wg is up. stop wg, restart nmb, start wg
 }
